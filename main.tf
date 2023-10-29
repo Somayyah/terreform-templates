95426917ab086438696741eeb43b34cd8dafdd15
sumaia <v-sumaia@microsoft.com>
2023-10-29 17:05:05 +0300

    added public ip

--- main.tf
+++ main.tf
@@ -35,18 +35,49 @@ resource "azurerm_subnet" "watari-ai-subnet" {
 }
 
 resource "azurerm_network_security_group" "watari-ai-sg" {
-  name = "watari-ai-sg"
+  name                = "watari-ai-sg"
   resource_group_name = azurerm_resource_group.watari-ai.name
-  location = azurerm_resource_group.watari-ai.location
-  tags = azurerm_resource_group.watari-ai.tags
+  location            = azurerm_resource_group.watari-ai.location
+  tags                = azurerm_resource_group.watari-ai.tags
 }
 
 resource "azurerm_network_security_rule" "watari-ai-sgr1" {
-  name = "watari-ai-sgr1"
-  resource_group_name = azurerm_resource_group.watari-ai.name
+  name                        = "watari-ai-sgr1"
+  resource_group_name         = azurerm_resource_group.watari-ai.name
   network_security_group_name = azurerm_network_security_group.watari-ai-sg.name
-  protocol = "Icmp"
-  access = "Deny"
-  priority = 100
-  direction = "Inbound"
+  protocol                    = "Icmp"
+  access                      = "Deny"
+  priority                    = 100
+  direction                   = "Inbound"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+}
+
+resource "azurerm_network_security_rule" "watari-ai-sgr2" {
+  name                        = "watari-ai-sgr2"
+  resource_group_name         = azurerm_resource_group.watari-ai.name
+  network_security_group_name = azurerm_network_security_group.watari-ai-sg.name
+  priority                    = 101
+  direction                   = "Outbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+}
+
+resource "azurerm_subnet_network_security_group_association" "watari-ai-s-n-sg-a" {
+  subnet_id                 = azurerm_subnet.watari-ai-subnet.id
+  network_security_group_id = azurerm_network_security_group.watari-ai-sg.id
+}
+
+resource "azurerm_public_ip" " watari-ai-ip" {
+  name                = "watari-ai-public-ip"
+  resource_group_name = azurerm_resource_group.watari-ai.name
+  location            = azurerm_resource_group.watari-ai.location
+  allocation_method   = "Static"
+  tags = azurerm_resource_group.watari-ai.tags
 }
