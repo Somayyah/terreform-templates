f9161a8a192c7a7113c24d983cdc63011340db8b
sumaia <v-sumaia@microsoft.com>
2023-10-29 15:35:31 +0300

    created vnet

--- main.tf
+++ main.tf
@@ -19,6 +19,16 @@ resource "azurerm_resource_group" "vision2" {
   }
 }
 
+resource "azurerm_virtual_network" "vision2_vn" {
+  name = "vision2_vn"
+  tags = {
+    environment : "development"
+  }
+  resource_group_name = azurerm_resource_group.vision2.name
+  location = azurerm_resource_group.vision2.location
+  address_space = ["10.0.0.0/16"]
+}
+
 
 
 
