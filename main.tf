66f5ab05a91372110258e09fad84dfa448f7984a
sumaia <v-sumaia@microsoft.com>
2023-10-30 12:29:49 +0300

    added new linux VM with SSH key for authentication

--- main.tf
+++ main.tf
@@ -86,7 +86,7 @@ resource "azurerm_public_ip" "watari-ai-ip" {
   tags                = azurerm_resource_group.watari-ai.tags
 }
 
-resource "azurerm_network_interface" "watari-ai-nic" { 
+resource "azurerm_network_interface" "watari-ai-nic" {
   name                = "watari-ai-nic"
   resource_group_name = azurerm_resource_group.watari-ai.name
   location            = azurerm_resource_group.watari-ai.location
@@ -96,5 +96,33 @@ resource "azurerm_network_interface" "watari-ai-nic" {
     subnet_id                     = azurerm_subnet.watari-ai-subnet.id
     private_ip_address_allocation = "Dynamic"
   }
+  tags = azurerm_resource_group.watari-ai.tags
+}
 
+resource "azurerm_linux_virtual_machine" "watari-ai-linux-vm" {
+  name                = "watari-ai-linux-vm"
+  resource_group_name = azurerm_resource_group.watari-ai.name
+  location            = azurerm_resource_group.watari-ai.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.watari-ai-nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/vm_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
 }
