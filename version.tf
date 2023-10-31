d66c04d7e4087d2dcb45a7a81a820381945963e8
sumaia <v-sumaia@microsoft.com>
2023-10-31 16:50:45 +0300

    updated providers

--- version.tf
+++ version.tf
@@ -1,8 +1,14 @@
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 1.3.0"
+
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.0"
     }
   }
 }
