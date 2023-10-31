7cd7165172c2272d88d4ee011557caa20e4aaee8
sumaia <v-sumaia@microsoft.com>
2023-10-31 16:36:49 +0300

    defined symlinks while checking if they already exist

--- OpenAI-STT-TTS/deploy.ps1
+++ OpenAI-STT-TTS/deploy.ps1
@@ -1,3 +1,22 @@
+# Define symlinks
+# Check if version.tf does not exist
+if (-not (Test-Path "version.tf")) {
+    New-Item -ItemType SymbolicLink -Name "version.tf" -Target "..\version.tf"
+}
+
+# Check if providers.tf does not exist
+if (-not (Test-Path "providers.tf")) {
+    New-Item -ItemType SymbolicLink -Name "providers.tf" -Target "..\providers.tf"
+}
+
+# Check if resource_group.tf does not exist (assuming you also want to create this symlink based on your earlier messages)
+if (-not (Test-Path "resource-group.tf")) {
+    New-Item -ItemType SymbolicLink -Name "resource-group.tf" -Target "..\resource-group.tf"
+}
+
+# Run terraform init
+terraform init
+<# 
 # Run terraform apply
 terraform apply -auto-approve
 
@@ -10,4 +29,4 @@ if (Test-Path .env) {
 $key = terraform output -raw storage_account_primary_access_key
 "STORAGE_ACCOUNT_PRIMARY_ACCESS_KEY=$key" | Out-File .env -Append
 
-# Add similar lines for other keys you want to save
+# Add similar lines for other keys you want to save #>
